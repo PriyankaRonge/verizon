b1f6e9030476c7315909a818629b28f4f9855b25
PriyankaRonge <rongepriyanka11@gmail.com>
2020-03-24 16:57:05 +0530

    changed value for addition

--- Operation/src/com/ivl/MathOperation.java
+++ Operation/src/com/ivl/MathOperation.java
@@ -3,7 +3,7 @@ package com.ivl;
 public class MathOperation {
 
 	public static void main(String[] args) {
-		int a= 10+90;
+		int a= 190+90;
 		System.out.println("addition :"+a);
 		int b= 190-90;
 		System.out.println("substraction :"+b);
