2c7b4460cacde438be80b9f63793aa109933d82d
PriyankaRonge <rongepriyanka11@gmail.com>
2020-03-25 13:03:57 +0530

    this is second commit for substraction...............

--- Calculation/src/com/ivl/Calculate.java
+++ Calculation/src/com/ivl/Calculate.java
@@ -5,6 +5,8 @@ public class Calculate {
  public static void main(String[] args) {
 int c = 10+90;
 System.out.println("addition :"+c);
+int s = 90-80;
+System.out.println("substraction :"+s);
 
  }
 }
