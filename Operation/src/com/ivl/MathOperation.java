3b8b3e0e2cfbc78235ffc431ee5d0c3d4d3a9ca1
PriyankaRonge <rongepriyanka11@gmail.com>
2020-03-25 16:29:23 +0530

    commit changes 2

--- Operation/src/com/ivl/MathOperation.java
+++ Operation/src/com/ivl/MathOperation.java
@@ -5,5 +5,9 @@ public class MathOperation {
 	public static void main(String[] args) {
 		int a= 10+90;
 		System.out.println("addition :"+a);
+		int b= 190-90;
+		System.out.println("substraction :"+b);
+		
+		
 	}
 }
