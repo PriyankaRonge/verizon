5381f62ab7195896d7997f84c0e7e4f4d846fca9
PriyankaRonge <rongepriyanka11@gmail.com>
2020-03-25 16:26:26 +0530

    commiting and pushing changes first

--- Calculation/src/com/ivl/calculate.java
+++
@@ -1,8 +0,0 @@
-package com.ivl;
-
-public class calculate {
-	public static void main(String[] args) {
-		int a=10+90;
-		System.out.println(" addition :"+a);
-	}
-}
---
+++ Operation/src/com/ivl/MathOperation.java
@@ -0,0 +1,9 @@
+package com.ivl;
+
+public class MathOperation {
+
+	public static void main(String[] args) {
+		int a= 10+90;
+		System.out.println("addition :"+a);
+	}
+}
